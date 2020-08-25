2ef7b94b5c5abcbf9209ff2e0a9f9f320bcdf409
dksxoghl <dksxoghl8@gmail.com>
2020-08-25 17:29:24 +0900

    시큐리티 설정파일 추가

--- src/main/java/com/taehi/springfirst/config/SecurityConfig.java
+++ src/main/java/com/taehi/springfirst/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package com.taehi.springfirst.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
