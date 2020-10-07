18f88fd7708cdc0157c9e379fef407577bf7ee27
dksxoghl <dksxoghl8@gmail.com>
2020-10-07 15:33:03 +0900

    개선:#9 CI

--- src/test/java/com/taehi/springfirst/SpringFirstApplicationTest.java
+++ src/test/java/com/taehi/springfirst/SpringFirstApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringFirstApplicationTests {
+class SpringFirstApplicationTest {
 
     @Test
     void contextLoads() {
